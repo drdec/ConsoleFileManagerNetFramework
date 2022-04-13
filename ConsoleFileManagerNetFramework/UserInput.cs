e47bf99d3e0eb0d102901383ac0a995108bacd02
DrDeckto <fynjy138@gmail.com>
2022-04-13 16:23:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleFileManagerNetFramework/UserInput.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleFileManagerNetFramework
+{
+    public class UserInput
+    {
+        private string _consoleCommand;
+
+        public UserInput(string str)
+        {
+            _consoleCommand = str;
+        }
+
+        public void 
+    }
+}
